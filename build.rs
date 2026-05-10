d876bb430068cd1ab216a2a712737e8be5ce502f
potoo0 <wqaz1413@gmail.com>
2026-05-10 22:00:13 +0800

    build(deps): replace vergen-gix with vergen-gitcl to reduce vulnerable deps

--- build.rs
+++ build.rs
@@ -1,8 +1,8 @@
 use anyhow::Result;
-use vergen_gix::{BuildBuilder, Emitter, GixBuilder};
+use vergen_gitcl::{BuildBuilder, Emitter, GitclBuilder};
 
 fn main() -> Result<()> {
     let build = BuildBuilder::all_build()?;
-    let gix = GixBuilder::all_git()?;
-    Emitter::default().add_instructions(&build)?.add_instructions(&gix)?.emit()
+    let git = GitclBuilder::all_git()?;
+    Emitter::default().add_instructions(&build)?.add_instructions(&git)?.emit()
 }
